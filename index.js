e969794ae888e637b09e5090f1da56fd8c07f66c
Dominique Paster <dpaster1@gmail.com>
2022-08-01 18:43:05 -0400

    Added log statement

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ let buttons = Array.from(document.getElementsByClassName ('button'));
 
 buttons.map ( buttons => {
     buttons.addEventListener('click', (e) => {
+        console.log(e.target.innerText);
         switch(e.target.innerText){
             case 'C':
                 display.innerText = '';
+                // break; without break it will run the below as well
             default:
                 display.innerText += e.target.innerText
         }
