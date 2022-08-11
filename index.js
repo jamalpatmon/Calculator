7c7a8fa0754ac6c5d54eab0bb1c0eb05269314fa
jamalpatmon <69773849+jamalpatmon@users.noreply.github.com>
2022-08-11 19:43:21 -0400

    Update index.js
    Finished adding breaks, try and catch as well as other code to make sure calculator app works properly.

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ buttons.map ( buttons => {
         switch(e.target.innerText){
             case 'C':
                 display.innerText = '';
-                // break; without break it will run the below as well
+                break;
+                case '←':
+                    if(display.innerText){
+                display.innerText = display.innerText.slice(0, -1);
+            }
+                break;
+                case '=':
+                    try{
+                        display.innerText = eval(display.innerText);
+                    } catch {
+                        display.innerText = 'Error!';
+                    }
+                    display.innerText = eval(display.innerText);
+                    break;
             default:
-                display.innerText += e.target.innerText
+                display.innerText += e.target.innerText;
         }
     })
 })
